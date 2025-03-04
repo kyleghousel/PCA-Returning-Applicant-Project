d5850a1e3c0065243abe4a1d91e5137f11496675
kyleghousel <kylegranthousel@gmail.com>
2025-03-03 22:45:05 -0500

    Updates

--- src/components/header.jsx
+++
@@ -1,7 +0,0 @@
-const Header = () => {
-    return (
-        <header className="w-screen flex static justify-center text-5xl p-10"><a href="/">Kyle Housel</a></header>
-    );
-}
-
-export default Header;
