31690bbadc815eda5e74d338053e5b4db7e2ef1a
kyleghousel <kylegranthousel@gmail.com>
2025-03-03 22:54:41 -0500

    Still trying to get github pages deploy right

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import Landing from './components/landing'
 import Bio from './components/bio'
